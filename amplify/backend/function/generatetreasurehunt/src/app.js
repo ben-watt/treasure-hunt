638d189067bf0de35d704f14d8954f8f391ded95
ben.watt <ben.watt@moonpig.com>
2022-12-30 10:40:01 +0000

    Update the lambda function to make a request to open api - NO API KEY

--- amplify/backend/function/generatetreasurehunt/src/app.js
+++ amplify/backend/function/generatetreasurehunt/src/app.js
@@ -9,6 +9,7 @@ See the License for the specific language governing permissions and limitations 
 const express = require('express')
 const bodyParser = require('body-parser')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const { forEachChild } = require('typescript')
 
 // declare a new express app
 const app = express()
@@ -25,13 +26,49 @@ app.use(function(req, res, next) {
 /****************************
 * Example post method *
 ****************************/
+const generate = async (req, res) => {
+let body = `You will play the role of TreasureHuntBot, you take in a list of items and return a rhyming clue for each. The clue must not contain the item, it must be a single sentence, it must rhyme and it should be a puzzle. For example:
 
-app.post('/generate', function(req, res) {
+  User: 
+  Lounge chair, in the dining room
+  Under the black dining table in the kitchen
+  At the back of the greenhouse under a brown pot
+  
+  TreasureHuntBot:
+  Find a spot to sit and stop, the treasure is hidden in that spot
+  Has four legs and is black, it's under that!
+  Where plants are nurtured and given care, the treasure is hidden somewhere there
+  
+  User: 
+  Behind the computer
+  Under the stairs
+  Behind the couch
+  
+  TreasureHuntBot:
+  You'll find the next clue where the screen is bright and blue
+  Up and down you'll search today but underneath is where it lays
+  Comfy cosy a place to sit, the clue lies behind this bit\n\nUser:\n`
+
+
+  for(let item in req.body) {
+    body += item.description
+  }
+  
+  
+  let request = { headers: { "Authorization" : `Bearer ${apiKey}` }, method: "POST", body: body };
+  let res = await fetch("https://api.openai.com/v1/completions", request);
+  console.log(res)
+  return res
+}
+
+
+app.post('/generate', async function(req, res) {
+  let res = await generate(req, res)
   res.json({success: 'post call succeed!', url: req.url, body: req.body})
 });
 
-app.post('/generate/*', function(req, res) {
-  // Add your code here
+app.post('/generate/*', async function(req, res) {
+  let res = generate(req, res)
   res.json({success: 'post call succeed!', url: req.url, body: req.body})
 });
 
